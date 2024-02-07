a7755206e9a0a5d5d83f91139475240bc1143a04
Ultra-The-Dev <108840468+Ultra-The-Dev@users.noreply.github.com>
2024-02-07 07:50:09 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Ready To Help","Theoimpact.org","Teen Health Empowerment Organization"];
 
 
 let currentIndex = 0;
